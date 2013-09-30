2d829df0428c29b0305c0967d6f94d8ca8177225
aaronpoweruser <atlrudeboi1@gmail.com>
2013-09-30 14:02:20 -0400

    Initial parent controller
    supports adding and removing subsciptoins subscriptions
    
    To intilize the id of the iframe defined in appconstants (must match dom id) is sent to the parent.
    The parent makes a frame object with the list of subscriptions (defined in appConstants).

--- examples/iframe-sandbox-puremvc/src/view/mediator/IFrameMediator.js
+++ examples/iframe-sandbox-puremvc/src/view/mediator/IFrameMediator.js
@@ -3,86 +3,109 @@
  * @extends puremvc.Mediator
  */
 puremvc.define(
-// CLASS INFO
-{
-    name: 'demo.view.mediator.IFrameMediator',
-    parent: puremvc.Mediator
-},
-// INSTANCE MEMBERS
-{ /** @override */
-    listNotificationInterests: function() {
-        return [
-        demo.AppConstants.SEND]
+    // CLASS INFO
+    {
+        name: 'demo.view.mediator.IFrameMediator',
+        parent: puremvc.Mediator
     },
-    /** @override */
-    handleNotification: function(note) {
-        switch (note.getName()) {
-        case demo.AppConstants.SEND:
-            console.log('Parent:dispatched to iframe \n' + note.getBody());
-           
-            for (i = 0; i < frames.length; i++) {
-               // frames[i].contentWindow.postMessage(note.getBody(), "*");
+    // INSTANCE MEMBERS
+    { /** @override */
+        listNotificationInterests: function ()
+        {
+            return [
+                demo.AppConstants.SEND]
+        },
+        /** @override */
+        handleNotification: function (note)
+        {
+            switch (note.getName())
+            {
+            case demo.AppConstants.SEND:
+                console.log('Parent:dispatched to iframe \n' + note.getBody());
+                s
+                break;
             }
-            break;
+        },
+        /** @override */
+        onRegister: function ()
+        {
+            var iframes = [];
+            var numFrames = 0;
+            getSubs();
+
+            function addListener()
+            {
+                var that = this;
+                window.addEventListener("message", function (e)
+                {
+                    console.log('Parent: recived msg:\n', e.data);
+                    console.log('Parent: Number of frames\n', numFrames);
+                    var data = e.data.split("|");
+                    if (data[0] = 0)
+                    {
+                        that.sendNotification(demo.AppConstants.PROCESS_TEXT, "internal");
+                    }
+                    else
+                    {
+                        iframes.node[data[0]].contentWindow.postMessage(data[1], "*");
+                    }
+                }, false);
+            }
+
+            function getSubs()
+            {
+                var that = this;
+                window.addEventListener("message", function (e)
+                    {
+                        console.log('Parent: recived msg:\n', e.data);
+                        var data = e.data.split("|");
+                        if (data[0].substr(0, 4) == "iframe")
+                        {
+                            iframes[numFrames] = new Object();
+                            iframes[numFrames].name = data[0];
+                            iframes[numFrames].node = document.getElementById(data[0]);
+                            iframes[numFrames].subs = [];
+                            for (i = 1; i < data.length; i++)
+                            {
+                                iframes[numFrames].subs[i - 1] = data[i];
+                            }
+                            numFrames++;
+                        }
+                        else if (data[0].substr(0, 3) == "type")
+                        {
+                            switch (data[1])
+                            {
+                            case "fiveormore":
+                                for (i = 0; i < numFrames; i++)
+                                {
+                                    for (n = 0; n < numFrames; n++)
+                                    {
+                                        if (iframes[i].subs[n] = "fiveormore")
+                                        {
+                                            iframes[i].node.contentWindow.postMessage(data[2], "*");
+                                        }
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        else if (data[0].substr(0, 4) == "remove")
+                        {
+                            iframes.splice(iframes.indexOf(data[1]), 1);
+                            numFrames--;
+                        }
+                }, false);
         }
     },
     /** @override */
-    onRegister: function() {
-        getSubs();
-       function addListener() {
-        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        var eventer = window[eventMethod];
-        var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-        var that = this;
-        eventer(messageEvent, function(e) {
-            console.log('Parent: recived msg:\n', e.data);
-   
-            framess[0] = document.getElementById("iframe");
-            framess[1] = document.getElementById("iframe2");
-            var data = e.data.split("|");
-            if(data[0] = 0) {
-                that.sendNotification(demo.AppConstants.PROCESS_TEXT, "internal");
-            } else {
-                frames[data[0]].contentWindow.postMessage(data[1], "*");
-            }
-
-        }, false);
-    }
-
-  function getSubs() {
-        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        var eventer = window[eventMethod];
-        var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-        var that = this;
-        eventer(messageEvent, function(e) {
-            console.log('Parent: recived msg:\n', e.data);
-
-           var data = e.data.split("|");
-           var iframes = [];
-            var numFrames = 0;
-            iframes[numFrames] = new Object();
-           iframes[numFrames].name = data[0];
-            iframes[numFrames].node = document.getElementById(data[0]);
-           iframes[numFrames].subs = [];
-           for (i =1; i < data.length ; i++) {
-                iframes[numFrames].subs[i-1] = data[i];
-            }
-            numFrames++;
-        }, false);
-    }
-    },
-    /** @override */
-    onRemove: function() {
+    onRemove: function ()
+    {
         frame.contentWindow.postMessage("PARENT_REMOVED", "*");
     },
-
-    
-},
 // STATIC MEMBERS
-{
-    /**
-     * @static
-     * @type {string}
-     */
-    NAME: 'IFrameMediator'
+/**
+ * @static
+ * @type {string}
+ */
+NAME: 'IFrameMediator'
 });
